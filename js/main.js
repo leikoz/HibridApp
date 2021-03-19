854a902fba73e1ed51b211ce0d5feeba9b50fae0
Dani <dani22hercha@hotmail.com>
2021-03-19 02:47:06 -0500

    Cambios en Tablas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var login, home, registro, perfil, configP;
+var cultural, social, comercial, deportivo, educativo, tecnologico, politico, cientifico;
 var secciones = [];
+
 
 window.onload = () => {
 	crearReferencias();
 	agregarEventos();
-	setTimeout(()=>{irA(login)},0);
+	setTimeout(()=>{irA(login)},0);	
 	
 }
 
@@ -14,9 +16,19 @@ function crearReferencias(){
 	registro = document.getElementById("form-sign-up");
 	perfil = document.getElementById("form-perfil");
 	configP = document.getElementById("form-config");
-	secciones = [login,home,registro,perfil,configP];
-
-
+	
+	
+	//Secciones Eventos
+	cultural	= document.getElementById("viewCulturales");
+	social		= document.getElementById("viewSociales");
+	comercial	= document.getElementById("viewComerciales");
+	deportivo	= document.getElementById("viewDeportivos");
+	educativo	= document.getElementById("viewEducativos");
+	tecnologico = document.getElementById("viewTecnologicos");
+	politico	= document.getElementById("viewPoliticos");
+	cientifico	= document.getElementById("viewCientificos");
+	
+	secciones = [login,home,registro,perfil,configP,cultural,social,comercial,deportivo,educativo,tecnologico,politico,cientifico];
 	//Botones
 
 	btn_ingresar 	= document.getElementById("btn_Ingresar");
@@ -29,6 +41,28 @@ function crearReferencias(){
 	btn_atrasC		= document.getElementById("btn_AtrasC");
 	btn_config 		= document.getElementById("btn_Config");
 	btn_perfil 		= document.getElementById("btn_Perfil");
+
+	btn_cultural	= document.getElementById("btn_Cultural");
+	btn_social		= document.getElementById("btn_Social");
+	btn_comercial 	= document.getElementById("btn_Comercial");
+	btn_deportivo	= document.getElementById("btn_Deportivo");
+	btn_educativo	= document.getElementById("btn_Educativo");
+	btn_tecnologico	= document.getElementById("btn_Tecnologico");
+	btn_politico	= document.getElementById("btn_Politico");
+	btn_cientifico	= document.getElementById("btn_Cientifico");
+
+	btn_atrasVEC	= document.getElementById("btn_AtrasVEC");
+	btn_atrasVES	= document.getElementById("btn_AtrasVES");
+	btn_atrasVECo	= document.getElementById("btn_AtrasVECo");
+	btn_atrasVED	= document.getElementById("btn_AtrasVED");
+	btn_atrasVEE	= document.getElementById("btn_AtrasVEE");
+	btn_atrasVET	= document.getElementById("btn_AtrasVET");
+	btn_atrasVEP	= document.getElementById("btn_AtrasVEP");
+	btn_atrasVECi	= document.getElementById("btn_AtrasVECi");
+
+
+
+
 }
 
 function agregarEventos(){
@@ -62,6 +96,55 @@ function agregarEventos(){
 	btn_perfil.addEventListener("click", () => {
         irA(perfil);});
 
+	//--\\
+
+	btn_cultural.addEventListener("click", () => {
+        irA(cultural);});
+	
+	btn_social.addEventListener("click", () => {
+        irA(social);});
+
+	btn_comercial.addEventListener("click", () => {
+        irA(comercial);});
+
+	btn_deportivo.addEventListener("click", () => {
+        irA(deportivo);});
+
+	btn_educativo.addEventListener("click", () => {
+        irA(educativo);});
+
+	btn_tecnologico.addEventListener("click", () => {
+        irA(tecnologico);});
+
+	btn_politico.addEventListener("click", () => {
+        irA(politico);});
+
+	btn_cientifico.addEventListener("click", () => {
+        irA(cientifico);});
+
+	btn_atrasVEC.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVES.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVECo.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVED.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVEE.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVET.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVEP.addEventListener("click", () => {
+        irA(home);});
+
+	btn_atrasVECi.addEventListener("click", () => {
+        irA(home);});
 
 
 }
@@ -76,6 +159,8 @@ function irA(seccion){
 	ocultarSecciones();
 	seccion.style.display = 'block';
 }
+
+
 
 
 
